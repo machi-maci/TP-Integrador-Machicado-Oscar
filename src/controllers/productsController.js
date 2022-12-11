9672281e203b692793592986e4b228610ed88386
oscar machicado <machicado.macias@gmail.com>
2022-12-11 04:59:34 -0300

    Listado 3/4

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,8 +1,18 @@
+const path = require('path');
+const fs = require('fs');
+
+const pathJson = path.resolve(__dirname, '../data/products.json');   //agrega la lista json de productos
+const productsJson = fs.readFileSync(pathJson, 'utf-8');
+const products = JSON.parse(productsJson);
+
 const { render } = require("ejs")
 
 let controller = {
     home: (req, res) => {
-        res.render('home');
+        res.render('home', {productos: products});
+    },
+    detail: (req, res) => {
+        res.render('detail', {productos: products});
     }
 }
 
--- src/routes/products.js
+++ src/routes/products.js
@@ -3,7 +3,7 @@ const controller = require('../controllers/productsController');
 const router = express.Router();
 
 
-router.get('/', controller.home)
-
+router.get('/', controller.home);
+router.get('/detail', controller.detail); /// se conecta con linea 7 productsController.js
 
 module.exports = router;
